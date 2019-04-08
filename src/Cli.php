8d7cafd127406942a37b65e1322db8d2336ac51e
kulikov98 <kulikov98@gmail.com>
2019-04-08 22:14:24 +0300

    add makefile

--- src/Cli.php
+++ src/Cli.php
@@ -5,18 +5,7 @@ namespace Gendiff\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
-function run() {
-    $doc = <<<DOC
-    Generate diff
-
-    Usage:
-        gendiff (-h|--help)
-        gendiff [--format <fmt>] <firstFile> <secondFile>
-
-    Options:
-        -h --help                     Show this screen
-        --format <fmt>                Report format [default: pretty]
-DOC;
-
+function run($doc)
+{
     $args = (new \Docopt\Handler)->handle($doc);
-}    
+}

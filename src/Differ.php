4b31f4e8f2c67d08b8f98c6f13e1c37740d6209c
kulikov98 <kulikov98@gmail.com>
2019-04-11 23:10:45 +0300

    refactoring

--- src/Differ.php
+++ src/Differ.php
@@ -2,7 +2,6 @@
 
 namespace Differ;
 
-use function Functions\genDiffText;
 use function Parser\parse;
 
 function genDiff($firstPath, $secondPath)
@@ -17,4 +16,39 @@ function genDiff($firstPath, $secondPath)
     $files = parse($firstFileExt, $firstPath, $secondPath);
 
     return genDiffText($files['first'], $files['second']);
+}
+
+function genDiffText($firstFileJson, $secondFileJson)
+{
+    $firstFile = json_decode($firstFileJson, true);
+    $secondFile = json_decode($secondFileJson, true);
+
+    $allKeys = array_keys(array_merge($firstFile, $secondFile));
+    $uniqueKeys = array_unique($allKeys);
+
+    $res = array_reduce($uniqueKeys, function ($acc, $item) use ($firstFile, $secondFile) {
+        // deleted value
+        if (key_exists($item, $firstFile) && !key_exists($item, $secondFile)) {
+            $acc[] = "- {$item}: {$firstFile[$item]}";
+        }
+        // new value
+        if (!key_exists($item, $firstFile) && key_exists($item, $secondFile)) {
+            $acc[] = "+ {$item}: {$secondFile[$item]}";
+        }
+        if (key_exists($item, $firstFile) && key_exists($item, $secondFile)) {
+            // same value
+            if ($firstFile[$item] === $secondFile[$item]) {
+                $acc[] = "  {$item}: {$firstFile[$item]}";
+                // changed value
+            } else {
+                $acc[] = "+ {$item}: {$secondFile[$item]}";
+                $acc[] = "- {$item}: {$firstFile[$item]}";
+            }
+        }
+        return $acc;
+    }, ['{']);
+
+    $res[] = '}';
+
+    return implode(PHP_EOL, $res);
 }
